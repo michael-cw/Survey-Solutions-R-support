43f473ae9a87865f76593f5249714b4c2fd46c60
michael-cw <wild.michael@gmail.com>
2018-04-08 16:55:26 +0000

    User management API

---
+++ suso_api_user.R
@@ -0,0 +1,44 @@
+################
+## API USER management
+
+
+## Load the libraries
+library("httr")
+library("xml2")
+library("jsonlite")
+library("readstata13")
+
+getSV<-function(url="https://xx.mysurvey.solutions",
+                usr="ApiUser",
+                pass="ApiPass"){
+  ##  Define the api 
+  url=paste0(url, "/api/v1/supervisors")
+  print(url)
+  test_detail<-GET(url = paste0(url, "?limit=200"), 
+                   authenticate(usr, pass, type = "basic"))
+  aJsonFile<-tempfile()
+  writeBin(content(test_detail, "raw"), aJsonFile)
+  test_json<-fromJSON(aJsonFile)
+  return(test_json)
+}
+
+
+#####################################################
+##  Requires SV ID!!
+##    DF is Users, interviewer id/name is col UserID/UserName
+getINT<-function(url="https://xx.mysurvey.solutions",
+                 usr="ApiUser",
+                 pass="ApiPass",
+                 sv="sv_id"){
+  ##  Define the api 
+  url=paste0(url, "/api/v1/supervisors/")
+  test_detail<-GET(url = paste0(url,sv,"/interviewers?limit=200"), 
+                   authenticate(usr, pass, type = "basic"))
+  
+  aJsonFile<-tempfile()
+  writeBin(content(test_detail, "raw"), aJsonFile)
+  test_json<-fromJSON(aJsonFile)
+  return(test_json)
+  
+}
+
